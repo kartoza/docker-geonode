e6fa61ea616b5478e6a366a76b179e62bf1831da
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-03-29 22:24:47 +0700

    [develop] Add log output for image variant processing

--- src/develop/scripts/github_check_image_variant.sh
+++ src/develop/scripts/github_check_image_variant.sh
@@ -7,6 +7,7 @@ if [[ -n "$GITHUB_ACTIONS" ]]; then
   echo "Set output build variant."
 
   if [[ -d "${BUILD_DIRECTORY}/variants/${IMAGE_VARIANT}" ]]; then
+    echo "Image Variant overrides recipe found: ${IMAGE_VARIANT}"
     echo "::set-output name=IS_VARIANT_EXISTS::1"
   fi
 fi
