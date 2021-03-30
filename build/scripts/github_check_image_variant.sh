a5ed2c3f0a0783d55e791394c157b53e62734856
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-03-31 00:16:39 +0700

    [develop] fix image variant check

--- build/scripts/github_check_image_variant.sh
+++ build/scripts/github_check_image_variant.sh
@@ -6,7 +6,7 @@ if [[ -n "$GITHUB_ACTIONS" ]]; then
   echo "Github Actions detected."
   echo "Set output build variant."
 
-  if [[ -d "${BUILD_DIRECTORY}/variants/${IMAGE_VARIANT}" ]]; then
+  if [[ -d "${BUILD_DIRECTORY}/variants/${IMAGE_VARIANT}" && "${IMAGE_VARIANT}" -ne "default" ]]; then
     echo "Image Variant overrides recipe found: ${IMAGE_VARIANT}"
     echo "::set-output name=IS_VARIANT_EXISTS::1"
   fi
