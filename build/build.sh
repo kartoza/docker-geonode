2a7f50905716276180c757664600d54a0338141c
Rizky Maulana Nugraha <lana.pcfre@gmail.com>
2021-03-31 04:28:24 +0700

    generate build

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash shell.nix
 # Default canonical tag
-docker build -t "${DOCKERHUB_REPO}/${PROJECT_NAME}:${VERSION}" \
-  -f geonode/scripts/spcgeonode/django/Dockerfile \
-  geonode
+set -eux
+
+if [[ -n "${IMAGE_VARIANT}" ]]; then
+  docker build -t "${CANONICAL_IMAGE_TAG}" \
+    -f geonode/scripts/spcgeonode/django/${IMAGE_VARIANT}.Dockerfile \
+    --target ${IMAGE_VARIANT} \
+    geonode
+else
+  docker build -t "${CANONICAL_IMAGE_TAG}" \
+    -f geonode/scripts/spcgeonode/django/Dockerfile \
+    geonode
+fi
